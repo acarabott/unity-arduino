98124d7f402caff072c489fd2d866b240900be53
Arthur Carabott <arthurc@gmail.com>
2018-06-03 21:12:01 +0100

    first commit

---
+++ Assets/SerialReader.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Set this to .NET 2.0, (not .NET 2.0 Subset)
+// Edit > ProjectSettings > Player > ApiCompatibilityLevel
+using System.IO.Ports;
+
+[System.Serializable]
+public class SerialData
+{
+    public float potValue;
+    public bool isButtonDown;
+}
+
+
+public class SerialReader : MonoBehaviour {
+    public string serialPort = "/dev/cu.usbmodem145141";
+    public int baudRate = 9600;
+    public SerialData data;
+    public bool isConnected = false;
+
+    protected SerialPort serial;
+
+    void Connect() {
+        if (serial != null && serial.IsOpen) {
+            serial.Close();
+        }
+
+        serial = new SerialPort(serialPort, baudRate);
+
+        try {
+            serial.Open();
+        }
+        catch {
+            Debug.LogError("Could not connect to serial port " + serialPort);
+        }
+
+        isConnected = true;
+    }
+
+    void Start () {
+        Connect();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (serial == null) { Connect(); }
+
+        if (serial.IsOpen) {
+            string jsonString = serial.ReadLine();
+            data = JsonUtility.FromJson<SerialData>(jsonString);
+        }
+        else {
+            isConnected = false;
+            Connect();
+        }
+    }
+
+    void OnDestroy() {
+        serial.Close();
+        isConnected = false;
+    }
+}
---
+++ Assets/Sphere.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour {
+    public SerialReader serialReader;
+
+	void Update () {
+        if (serialReader && serialReader.isConnected) {
+            float scale = serialReader.data.potValue * 5.0f;
+            gameObject.transform.localScale = new Vector3(scale, scale, scale);
+
+            Renderer sphereRenderer = gameObject.GetComponent<Renderer>();
+            if (sphereRenderer && sphereRenderer.material) {
+                sphereRenderer.material.color = serialReader.data.isButtonDown
+                    ? Color.white
+                    : Color.black;
+            }
+        }
+	}
+}
