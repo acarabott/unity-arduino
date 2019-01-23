939bbcec6fd1f454279821ebcc10e11179d1da64
Arthur Carabott <arthurc@gmail.com>
2019-01-23 18:19:21 +0000

    arduino 07 01 separate getArmAngles file

--- 01-arduino/07-robot-arm/01-arm-position/src/main.cpp
+++ 01-arduino/07-robot-arm/01-arm-position/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <getArmAngles.h>
 
 // Topics
 // - structs
@@ -21,24 +22,6 @@ float xPos = 6.0f;
 float yPos = 10.0f;
 
 const float smoothing = 0.80f;
-
-struct armAngles {
-  float base;
-  float joint;
-};
-
-armAngles getArmAngles(float xPos, float yPos, float armLength) {
-  const float distanceToTarget = min(sqrtf(xPos * xPos + yPos * yPos), armLength * 2 * 0.95f);
-  const float alpha = xPos == 0 ? 0 : atanf(yPos / xPos) * RAD_TO_DEG;
-  const float beta = acosf(distanceToTarget / (2.0f * armLength)) * RAD_TO_DEG;
-
-  struct armAngles angles = {
-    .base = 180.0f - (alpha + beta),
-    .joint = 180.0f - (2.0f * beta),
-  };
-
-  return angles;
-}
 
 void setup() {
   baseServo.attach(baseServoPin);
