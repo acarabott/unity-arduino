14190837c4b45c6ed017a860e4c12028933a4019
Arthur Carabott <arthurc@gmail.com>
2019-02-10 23:12:57 +0000

    unity: add audio examples

---
+++ 02-unity/Intentional Interactions/Assets/Scripts/W2D3 Audio Input/AudioResponder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioResponder : MonoBehaviour
+{
+    public AudioAnalyser audioAnalyser;
+    public float scale = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (audioAnalyser == null)
+        {
+            Debug.LogError("no AudioAnalyser attached");
+            return;
+        }
+
+        float newScale = audioAnalyser.rootMeanSquared * scale;
+        gameObject.transform.localScale = new Vector3(newScale, newScale, newScale);
+    }
+}
