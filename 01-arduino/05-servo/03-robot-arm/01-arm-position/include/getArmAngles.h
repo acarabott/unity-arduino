416dfba14d3ceea0cbc20005ffd32e5919180c3d
Arthur Carabott <arthurc@gmail.com>
2019-03-04 14:51:31 +0000

    protect against / 0 in getArmAngles

--- 01-arduino/05-servo/03-robot-arm/01-arm-position/include/getArmAngles.h
+++ 01-arduino/05-servo/03-robot-arm/01-arm-position/include/getArmAngles.h
@@ -7,7 +7,7 @@ struct armAngles {
 
 armAngles getArmAngles(float xPos, float yPos, float armLength) {
   const float distanceToTarget = min(sqrtf(xPos * xPos + yPos * yPos), armLength * 2 * 0.95f);
-  const float alpha = xPos == 0 ? 0 : atanf(yPos / xPos) * RAD_TO_DEG;
+  const float alpha = xPos == 0 ? 0 : atanf(yPos / max(xPos, 0.001)) * RAD_TO_DEG;
   const float beta = acosf(distanceToTarget / (2.0f * armLength)) * RAD_TO_DEG;
 
   struct armAngles angles = {
